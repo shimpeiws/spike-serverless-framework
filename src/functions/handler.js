9e3274fc5fc3751e5a13cb35a163e344af394b5b
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-02-09 18:34:14 +0900

    Run in offline

--- src/functions/handler.js
+++ src/functions/handler.js
@@ -5,8 +5,8 @@ module.exports.hello = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
