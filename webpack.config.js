020e50993202cb56e98ffbaa424f995ac07e3075
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-02-09 20:33:48 +0900

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
