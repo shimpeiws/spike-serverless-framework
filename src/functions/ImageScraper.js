e205de39bb907ed4cc584b56b2bb2878676edea2
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-02-10 21:40:21 +0900

    Add ImageScraper

---
+++ src/functions/ImageScraper.js
@@ -0,0 +1,5 @@
+export default class ImageScraper {
+  static async images() {
+    return new Promise((resolve, reject) => resolve('Image Scraper!!!'));
+  }
+}
--- src/functions/handler.js
+++ src/functions/handler.js
@@ -1,3 +1,5 @@
+import ImageScraper from './ImageScraper';
+
 export const hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -11,4 +13,17 @@ export const hello = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+export const imageScraper = async (event, context, callback) => {
+  const message = await ImageScraper.images();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message,
+      input: event
+    })
+  };
+
+  callback(null, response);
 };
