25123e30e93a11fb070257d9f1094671e90cfd9e
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-02-11 01:01:17 +0900

    Setup dynamo

--- src/functions/ImageScraper.js
+++ src/functions/ImageScraper.js
@@ -1,8 +1,34 @@
 import puppeteer from 'puppeteer';
 import launchChrome from '@serverless-chrome/lambda';
 import CDP from 'chrome-remote-interface';
+import AWS from 'aws-sdk';
+import uuid from 'uuid';
 
 export default class ImageScraper {
+  static dynamoDb() {
+    return new AWS.DynamoDB.DocumentClient();
+  }
+  static async writeToDynamo(url, base64Image) {
+    const dynamo = this.dynamoDb();
+    const timestamp = new Date().getTime();
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        url,
+        base64Image,
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
+    };
+    await dynamo.put(params, error => {
+      // handle potential errors
+      if (error) {
+        console.error(error);
+        return;
+      }
+    });
+  }
   static async screenshot(url) {
     const slsChrome = await launchChrome();
     const browser = await puppeteer.connect({
@@ -14,6 +40,7 @@ export default class ImageScraper {
     await page.goto(url);
     await page.waitFor(1000);
     const res = await page.screenshot({ fullPage: false, type: 'jpeg', encoding: 'base64' });
+    await this.writeToDynamo(url, res);
     browser.close();
     return res;
   }
